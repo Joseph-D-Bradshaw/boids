2d9b9d99199d7b4fffcdbca85c0ce6a3e09f950b
Joseph-D-Bradshaw <joseph.bradshaw@outlook.com>
2025-12-13 23:21:48 +0100

    separation behaviour

--- boid.gd
+++ boid.gd
@@ -1,51 +1,57 @@
-extends Node2D
+extends CharacterBody2D
 
-const KNN = 5
+const EPS = 0.001
 
 var neighbours = []
 
 @export var IS_DEAD = false
 
-@export var SPEED = 100.0
-@export var SEPARATION_FACTOR = 1.0
+@export var MIN_SPEED = 20.0
+@export var MAX_SPEED = 200.0
+@export var SPEED = 60.0
+@export_range(100.0, 1000.0, 10.0) var SEPARATION_FACTOR = 500.0
 @export var ALIGNMENT_FACTOR = 25.0
 @export var COHESION_FACTOR = 50.0
 
 @onready var separation_area: Area2D = $SeparationArea
-@onready var static_body_2d: StaticBody2D = %StaticBody2D
 @onready var separation_circle: CollisionShape2D = $SeparationArea/SeparationCollision
+@onready var line_2d = $Line2D
+@onready var polygon_2d = $Polygon2D
 
-func _process(delta):
+func _ready():
+	velocity = Vector2(randf() - 0.5, randf() - 0.5) * MAX_SPEED
+
+func _process(_delta):
 	if IS_DEAD: return
-	global_position += transform.x * SPEED * delta
-	separation(delta)
-	
+	velocity *= SPEED
 
-func separation(delta):
+	if velocity.length() < MIN_SPEED:
+		velocity = velocity.normalized() * MIN_SPEED
+	elif velocity.length() > MAX_SPEED:
+		velocity = velocity.normalized() * MAX_SPEED
+
+	get_separation()
+	move_and_slide()
+
+	polygon_2d.look_at(global_position + velocity * 2)
+
+func get_separation():
 	var sep_neighbours = get_separation_neighbours()
+	
 	if sep_neighbours:
-		var dir_to_neighbours := Vector2.ZERO
+		var separation := Vector2.ZERO
+
 		for n in sep_neighbours:
-			dir_to_neighbours += n.global_position - global_position
-		var sep_dir := dir_to_neighbours * -1.0
-		var diff = abs(separation_circle.shape.radius - sep_dir.length())
-		var angle = atan2(sep_dir.y, sep_dir.x)
-		var eased_angle = rotate_toward(rotation, angle, delta * SEPARATION_FACTOR * atan(diff))
-		rotation = eased_angle
-	
+			var direction = (global_position - n.global_position)
+			var distance = direction.length()
+			var normalized_dir = direction / distance
+			var weight = 1 / (distance + EPS)
+			separation += normalized_dir * weight
+		
+		line_2d.points[0] = Vector2(0, 0)
+		line_2d.points[1] = separation
+
+		velocity += separation * SEPARATION_FACTOR
+
 func get_separation_neighbours():
-	return separation_area.get_overlapping_bodies().filter(func(b): return b != static_body_2d)
-
-func get_k_nearest(k: int):
-	# TODO: Improve this to use a radius filter to minimize calculations
-	var nearest = func(a: Node2D, b: Node2D):
-		var a_diff = global_position - a.global_position
-		var b_diff = global_position - b.global_position
-		return a if a_diff < b_diff else b
-	
-	#var local_boids = BoidManager.boids
-	#local_boids.sort_custom(nearest)
-	#return local_boids.slice(0, k)
-
-func calc_separation():
-	pass
+	return separation_area.get_overlapping_bodies().filter(func(b): return b != self)
--- main.gd
+++ main.gd
@@ -1,1 +1,20 @@
 extends Node2D
+
+@onready var boundary_top = $BoundaryTop
+@onready var boundary_bottom = $BoundaryBottom
+@onready var boundary_left = $BoundaryLeft
+@onready var boundary_right = $BoundaryRight
+
+func _ready():
+	boundary_top.body_entered.connect(func(body: Node2D):
+		body.velocity.y *= -1.0
+	)
+	boundary_bottom.body_entered.connect(func(body: Node2D):
+		body.velocity.y *= -1.0
+	)
+	boundary_left.body_entered.connect(func(body: Node2D):
+		body.velocity.x *= -1.0
+	)
+	boundary_right.body_entered.connect(func(body: Node2D):
+		body.velocity.x *= -1.0
+	)
